1294527989591b490e5fa942293cfba38d7dfa7c
ThePowerfulPas2 <Jonni.Jopie@gmail.com>
2021-03-18 11:12:35 +0000

    ny siða

---
+++ Verkefni00-HalloHeimur/sketch.js
@@ -0,0 +1,12 @@
+// Hér kemur kóðinn þinn:
+function setup(){
+    createCanvas(1535,750);
+    background(255,200,200);
+    fill(150);
+    ellipse(760,500,225,225);
+    fill(0);
+    textSize(80);
+    text("Velkomin/n á Vefsíðu mína",300,160);
+    rect(1200,420,150,150);
+    ellipse(250,500,150,150);
+}
