e150766b51adb8aceb7612045944670e56553b0d
ThePowerfulPas2 <Jonni.Jopie@gmail.com>
2021-03-24 14:53:01 +0000

    Verkefni01-Mondrain sett upp

---
+++ Verkefni01-Mondrain/sketch.js
@@ -0,0 +1,21 @@
+// Hér kemur kóðinn þinn:
+function setup() {
+  createCanvas(500,500);
+  background(0); //Svartur bakgrunnur
+  fill(200,0,0); //Rauður
+  rect(0,0,200,260);
+  fill(240); //Hvítur
+  rect(210,0,500,260);
+  fill(240); //Hvítur 2
+  rect(0,270,200,500);
+  fill(250); //"Hvítur"
+  rect(210,270,250,210);
+  fill(0,0,150); //Blár
+  rect(470,270,300,90);
+  fill(240); //Hvítur 3
+  rect(470,370,300,300);
+  fill(240,240,0); //Gulur
+  rect(210,490,140,300);
+  fill(20); //"Svartur"
+  rect(360,490,110,300);
+}
